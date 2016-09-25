a156e430e0441e86467e4eb6d22d8096a8fe1bd1
jaowl <fdudinskiy@gmail.com>
2016-09-25 11:01:24 +0300

    if-then множественное

--- sbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -10,21 +10,26 @@ public class Equation {
 	
 	private int n;
 	
-	public Equation(double  a, double b, double c){
+	public Equation(double a, double b, double c) {
 		this.a = a;
 		this.b = b;
 		this.c = c;
 		
-		double d=Math.pow(b,2) -4*a*c;
+		double d = Math.pow(b, 2) - 4 * a * c;
 		
-		if(d>0){
-			n=2;
-		}else if(d==0){
-			n=1;
-		}else if(d<0){
-			n=0;
+		if (0 != a) {
+			if (d > 0) {
+				n = 2;
+			} else if (d == 0) {
+				n = 1;
+			} else {
+				n = 0;
+			}
+		} else if (0 != b) {
+			n = 1;
+		} else {
+			n = (0 == c) ? -1 : 0;
 		}
-		
 	}
 	
 	public int getN() {
