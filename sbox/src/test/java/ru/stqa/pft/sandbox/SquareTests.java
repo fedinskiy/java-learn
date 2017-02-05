4eab3a63ea32442b8bf9723a7558e67bd57fba44
fedinskiy <fdudinskiy@gmail.com>
2017-02-05 10:02:08 +0300

    Правки для проверки соединения с гитхабом

--- sbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -10,9 +10,9 @@ import org.testng.annotations.Test;
 public class SquareTests {
 	@Test
 	public void testArea(){
-		Square s= new Square(10);
+		Square s= new Square(7);
 		
-		Assert.assertEquals(s.area(),100.0);
+		Assert.assertEquals(s.area(),49.0);
 	}
 }
  
