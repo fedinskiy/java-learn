db91c2936d3ff30e9df28301872a90c8fbe0b50b
fedinskiy <fdudinskiy@gmail.com>
2017-02-05 08:58:15 +0300

    Сборка версии на новой машине и изменения в SquareTests

--- sbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -10,9 +10,9 @@ import org.testng.annotations.Test;
 public class SquareTests {
 	@Test
 	public void testArea(){
-		Square s= new Square(5);
+		Square s= new Square(10);
 		
-		Assert.assertEquals(s.area(),25.0);
+		Assert.assertEquals(s.area(),100.0);
 	}
 }
  
